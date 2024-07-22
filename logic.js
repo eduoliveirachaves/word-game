7e4abe70aa17372d2de45aaeca08b92d7ef10c0d
edu <eduardodeoliveirachaves7@gmail.com>
2024-07-22 15:31:20 -0300

    mostly of html and css finished

--- logic.js
+++ logic.js
@@ -1,4 +1,5 @@
-function init() {
+const url = "https://words.dev-apis.com/word-of-the-day"
 
+await function init() {
+    fetch(url).then(res => res.json()).then(data => console.log(data));
 }
-init();
