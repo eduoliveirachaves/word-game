f5128b8406ae2aa1a5e74acec7bf6756e8801187
edu <eduardodeoliveirachaves7@gmail.com>
2024-07-23 21:22:37 -0300

    html and css finished and js lack api part
    in html and css part, I have finished the basic layout and style of the website, and changes the input type to divs to make just display the letters.
    in js part, I have finished the basic logic of the game, but the api part is not finished yet, so the game is not playable now.

--- logic.js
+++ logic.js
@@ -1,5 +1,76 @@
 const url = "https://words.dev-apis.com/word-of-the-day"
+let row = 0;
+let col = 1;
+const rows = [".one", ".two", ".three", ".four", ".five"];
+const validChars = ['Backspace', 'Enter'];
 
-await function init() {
-    fetch(url).then(res => res.json()).then(data => console.log(data));
+function init() {
+
+    //letter function
+    addEventListener("keydown", function (event) {
+
+        if (gameEnd() || !validInput(event.key)) {
+            return;
+        }
+        
+        if (event.key === "Backspace") {
+            backspace();
+            return;
+        }
+
+        if (col > 5) {
+            if (event.key === "Enter") {
+                //check if valid word
+                if (true) {
+                    //process and evaluate answer
+                    col = 1;
+                    row++;
+                    //check answer
+                    answerCheck()
+                }
+
+            }
+            
+            return;
+        }
+
+        document.querySelector(rows[row])
+            .querySelector(`.letter:nth-child(${col})`)
+            .innerText = event.key;
+
+        col++;
+    });
+
 }
+
+function backspace() {
+    if (col > 1) {
+        col--;
+    }
+    document.querySelector(rows[row])
+        .querySelector(`.letter:nth-child(${col})`)
+        .innerText = null;
+}
+
+function gameEnd() {
+    return row === 5;
+}
+
+function answerCheck () {
+    let correctWord = "";
+    let word = "";
+    for (let i = 1; i < 5; i++) {
+        word = document.querySelector(rows[row])
+            .querySelector(`.letter:nth-child(${i})`)
+            .innerText;
+    }
+}
+
+function validInput(input) {
+    return input.match(/^[a-zA-Z]$/) && validChars.includes(input);
+}
+
+
+
+
+init();
